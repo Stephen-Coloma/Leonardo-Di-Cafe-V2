48037d992f8c5a9a78c6718d17eb4e384a53528c
Dave14 <Dave14@gmail.com>
2024-03-19 09:45:14 +0900

    Serialize and save beverage menu to JSON file using Gson.

--- src/main/java/util/BeverageTester.java
+++ src/main/java/util/BeverageTester.java
@@ -26,7 +26,8 @@ public class BeverageTester {
         Beverage beverage, beverage1, beverage2, beverage3;
 
         try {
-            beverage = new Beverage("Matcha Frosted Iced Latte", 'b', 4.5, 23, new Object[]{"Black Iced Coffee.png", ImageUtility.getImageBytes("Black Iced Coffee.png")}, "A refreshing blend of rich coffee with a hint of chocolate, perfect for any time of the day.", 15, 10, 5, 10.0, 15.0, 20.0);
+            // Create beverages
+            beverage = new Beverage("Midnight Mocha Iced Coffee", 'b', 4.5, 23, new Object[]{"Black Iced Coffee.png", ImageUtility.getImageBytes("Black Iced Coffee.png")}, "A refreshing blend of rich coffee with a hint of chocolate, perfect for any time of the day.", 15, 10, 5, 10.0, 15.0, 20.0);
             beverage1 = new Beverage("Matcha Frosted Iced Latte", 'b', 4.4, 12, new Object[]{"Black Iced Coffee.png", ImageUtility.getImageBytes("Black Iced Coffee.png")}, "A refreshing combination of matcha and frosted milk, served over ice for a delightful treat.", 15, 10, 5, 10.0, 15.0, 20.0);
             beverage2 = new Beverage("Mocha Madness Frappe", 'b', 4.9, 24, new Object[]{"Mocha Frappe.png", ImageUtility.getImageBytes("Mocha Frappe.png")}, "Experience the ultimate mocha indulgence with this rich and creamy frappe.", 15, 10, 5, 10.0, 15.0, 20.0);
             beverage3 = new Beverage("Okinawa Delight Milk Tea", 'b', 4.2, 32, new Object[]{"Okinawa Milktea.png", ImageUtility.getImageBytes("Okinawa Milktea.png")}, "Transport yourself to Okinawa with this delightful milk tea, boasting rich and aromatic flavors.", 15, 10, 5, 10.0, 15.0, 20.0);
@@ -42,9 +43,15 @@ public class BeverageTester {
         beverageMenu.put("Okinawa Delight Milk Tea", beverage3);
 
         // Serialize and save the beverage menu to a JSON file
-        JSONUtility.saveBeverageMenu(beverageMenu, filepath);
-        System.out.println("Beverage menu saved successfully!");
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        String json = gson.toJson(beverageMenu);
 
+        try (FileWriter writer = new FileWriter(filepath)) {
+            writer.write(json);
+            System.out.println("Beverage menu saved successfully!");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     private static void testLoad(File file) {
